315e1950b579fba685facb1e7299be6fb18aa16c
30lima <99846550+Leonardo-dev-br@users.noreply.github.com>
2024-11-22 20:23:42 -0300

    WIP: Tentando pegasr dados do user

--- the-wind-eyes/src/app/Profile/page.tsx
+++ the-wind-eyes/src/app/Profile/page.tsx
@@ -3,19 +3,46 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import userIcon from "../assets/userIcon.png";
 import { useEffect, useState } from "react";
-import { useAuth } from "../../../Context/AuthContext"; 
 import { ContentParagraph } from "../components/ContentParagraph/ContentParagraph";
 
+type UserType = {
+  id: number;
+  nome: string;
+  sobrenome: string;
+  email: string;
+  dataDeNascimento: string;
+  cep: string;
+};
 
 type CepType = {
   neighborhood: string;
 };
 
 export default function Profile() {
-  const { user } = useAuth();
+  const [user, setUser] = useState<UserType | null>(null);
   const [cep, setCep] = useState<CepType | null>(null);
+  const userId = 1; 
 
   useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/usuario/${userId}`);
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data);
+        } else {
+          console.error(`Erro ao buscar usuário: ${response.statusText}`);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar usuário:", error);
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  useEffect(() => {
+    // Busca o CEP
     const searchCep = async () => {
       try {
         if (user?.cep) {
@@ -33,7 +60,11 @@ export default function Profile() {
     if (user?.cep) {
       searchCep();
     }
-  }, [user?.cep]); 
+  }, [user?.cep]);
+
+  if (!user) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <main className={styles.main}>
@@ -47,15 +78,15 @@ export default function Profile() {
               height={150}
             />
             <ContentParagraph color="white" fontSize="1.5rem">
-              {user?.nome} {user?.sobrenome}
+              {user.nome} {user.sobrenome}
             </ContentParagraph>
           </div>
           <div className={styles.infoDiv}>
-            <p className={styles.p}>Data de nascimento: {user?.dataDeNascimento}</p>
+            <p className={styles.p}>Data de nascimento: {user.dataDeNascimento}</p>
             <p className={styles.p}>
               Bairro: {cep?.neighborhood || "Bairro não encontrado"}
             </p>
-            <p className={styles.p}>Email: {user?.email}</p>
+            <p className={styles.p}>Email: {user.email}</p>
           </div>
         </div>
       </section>
